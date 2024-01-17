fde8f3f1719f26206d9c6729f4ecfa38e5faaac9
Adrian Chojnacki <adrian.chojnacki@fabrity.pl>
2024-01-17 15:12:41 +0100

    basics

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write("<h1>hello world</h1>");
+  res.write("<p>hello world</p>");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {

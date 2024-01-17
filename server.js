449cbe7a9ac9444f31719dbcd9c970920206b515
Adrian Chojnacki <adrian.chojnacki@fabrity.pl>
2024-01-17 15:17:39 +0100

    base

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   res.setHeader("Content-Type", "text/html");
-  res.write('<head><link rel="stylesheet" href="#"></head>');
-  res.write("<h1>hello world</h1>");
-  res.write("<p>hello world</p>");
-  res.end();
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {

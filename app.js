3308e3e391317fa4ea860af2e73c3e531aa6803c
Adrian Chojnacki <adrian.chojnacki@fabrity.pl>
2024-01-18 15:25:57 +0100

    ejs

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ const express = require("express");
 
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Lorem ipsum dolor", snippet: "Lorem ipsum dolor sit amet..." },
+    { title: "Lorem ipsum dolor", snippet: "Lorem ipsum dolor sit amet..." },
+    { title: "Lorem ipsum dolor", snippet: "Lorem ipsum dolor sit amet..." },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
+});
+
 app.use((req, res) => {
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });

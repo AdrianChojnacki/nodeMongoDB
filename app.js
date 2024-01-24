43ec537cc2dc04784b4e91efb81156b7d152376f
Adrian Chojnacki <adrian.chojnacki@fabrity.pl>
2024-01-24 17:04:53 +0100

    MongoDB

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
+
+const dbURI =
+  "mongodb+srv://<username>:<password>@tutorial.3io4uhs.mongodb.net/tutorial-db?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs");
 
-app.listen(3000);
-
 // middleware & static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("65b131d29ca077239a6ecbc5")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get("/", (req, res) => {
   const blogs = [
